4fc442d7802a018a26056b1a24b4c5480e826c5a
shan19990 <shankhanil.ghosh123@gmail.com>
2024-06-10 20:17:15 +0530

    Update

--- yaphets/wsgi.py
+++ yaphets/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'yaphets.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'yaphets.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "yaphets.settings")
 
 application = get_wsgi_application()
