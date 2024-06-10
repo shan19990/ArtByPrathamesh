75eab80f80c1019e48575bc087f10a3770db79f0
shan19990 <shankhanil.ghosh123@gmail.com>
2024-06-10 20:29:49 +0530

    Updated

--- yaphets/settings.py
+++ yaphets/settings.py
@@ -202,7 +202,13 @@ AUTH0_CLIENT_ID = "8zr0N9MOI30Ip7M06KEgDQYx02IgeK04"
 AUTH0_CLIENT_SECRET = "qowDbvM7NMk3r3c2R3w8N07rqEH_KNHFBwUIIgMLZElt8LFzXCG5QAMbxEvWu1g8"
 
 # Email configuration
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # TLS port
+EMAIL_HOST_USER = 'shankhanil.ghosh123@gmail.com'
+EMAIL_HOST_PASSWORD = 'ovbxdszhzidoluwt'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
